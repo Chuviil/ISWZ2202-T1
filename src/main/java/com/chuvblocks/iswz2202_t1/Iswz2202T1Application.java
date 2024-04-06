6283b46262470ec5785480a5645d3ece7436c340
Sebastian Quito <josue.quito@udla.edu.ec>
2024-04-06 17:39:33 -0500

    Initial Commit

---
+++ src/main/java/com/chuvblocks/iswz2202_t1/Iswz2202T1Application.java
@@ -0,0 +1,13 @@
+package com.chuvblocks.iswz2202_t1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iswz2202T1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Iswz2202T1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/chuvblocks/iswz2202_t1/Iswz2202T1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chuvblocks.iswz2202_t1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Iswz2202T1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
