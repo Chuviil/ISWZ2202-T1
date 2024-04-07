5a8a233c25f7d7fee0c3568b3f6acc708dfeb572
Sebastian Quito <josue.quito@udla.edu.ec>
2024-04-06 19:03:39 -0500

    Created Usuario Repository

---
+++ src/main/java/com/chuvblocks/iswz2202_t1/repository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package com.chuvblocks.iswz2202_t1.repository;
+
+import com.chuvblocks.iswz2202_t1.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
