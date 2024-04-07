ed23e6d2ada0c9b902058d6a241e48b39617e81d
Sebastian Quito <josue.quito@udla.edu.ec>
2024-04-06 21:27:01 -0500

    Created Usuario Service Interface

---
+++ src/main/java/com/chuvblocks/iswz2202_t1/service/IUsuarioService.java
@@ -0,0 +1,9 @@
+package com.chuvblocks.iswz2202_t1.service;
+
+import com.chuvblocks.iswz2202_t1.model.Usuario;
+
+public interface IUsuarioService {
+    Iterable<Usuario> getUsuarios();
+    Usuario createUsuario(Usuario usuario);
+    Usuario getUsuarioById(long id);
+}
