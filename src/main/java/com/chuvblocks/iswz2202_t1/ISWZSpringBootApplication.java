0e9da6728a418f2b47870d0c176d50ea0f32c748
Sebastian Quito <josue.quito@udla.edu.ec>
2024-04-06 19:03:00 -0500

    Renamed files with Subject ID

--- src/main/java/com/chuvblocks/iswz2202_t1/ISWZSpringBootApplication.java
+++ src/main/java/com/chuvblocks/iswz2202_t1/ISWZSpringBootApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Iswz2202T1Application {
+public class ISWZSpringBootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Iswz2202T1Application.class, args);
+        SpringApplication.run(ISWZSpringBootApplication.class, args);
     }
 
 }
